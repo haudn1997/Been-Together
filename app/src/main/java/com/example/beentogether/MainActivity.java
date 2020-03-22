95bc770f02f6ad54189d26fcdfe3112c60ed04eb
Hammer <tanhau1910@gmail.com>
2020-03-22 10:25:09 +0700

    Design Actionbar

--- app/src/main/java/com/example/beentogether/MainActivity.java
+++ app/src/main/java/com/example/beentogether/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.beentogether;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
